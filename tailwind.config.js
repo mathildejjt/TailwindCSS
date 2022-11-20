66b9b5a994984b85bc6751ef58e6a3b6ec8488c7
mathildejjt <mathilde@jajolet.fr>
2022-11-20 03:12:49 +0100

    ✨📱Contenu_terminé+Responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,13 +14,14 @@ module.exports = {
       colors: {
         'Soft_red': '#FE7866',
         'Yellow' : '#F9D400',
-        'Dark_desaturated_cyan' : '#24554B',
+        'Dark_desaturated_cyan' : '#386F63',
         'Dark_blue' : '#19526B',
         'Dark_moderate_cyan': '#458C7D',
         'Very_dark_desaturated_blue': '#232F3D',
         'Very_dark_grayish_blue': '#5A626C',
         'Dark_grayish_blue': '#808397',
         'Grayish_blue': '#A7AAAE',
+        'Grayish_cyan':'#7EBEB0',
       }
     },
   },
